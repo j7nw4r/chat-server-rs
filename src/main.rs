551cc605bd72504f2ec081d16daa7c67a5552d93
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2023-07-14 15:53:29 -0400

    Reader scaffold.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
 use axum::{
     extract::{
         ws::{Message, WebSocket},
-        ConnectInfo, WebSocketUpgrade,
+        WebSocketUpgrade,
     },
     http::StatusCode,
     response::IntoResponse,
     routing::get,
     Router, Server,
 };
-use futures::{
-    stream::{SplitSink, SplitStream, StreamExt},
-    TryStreamExt,
-};
+use futures::stream::{SplitSink, SplitStream, StreamExt};
 use std::net::SocketAddr;
+use tracing::debug;
 
 #[tokio::main]
 async fn main() {
@@ -40,9 +38,26 @@ async fn handle_chat(socket: WebSocket) {
     tokio::spawn(read_chat(reader));
 }
 
-async fn write_chat(writer: SplitSink<WebSocket, Message>) {}
+async fn write_chat(mut writer: SplitSink<WebSocket, Message>) {}
 
-async fn read_chat(reader: SplitStream<WebSocket>) {}
+async fn read_chat(mut reader: SplitStream<WebSocket>) -> anyhow::Result<()> {
+    fn process(message: Message) {}
+
+    loop {
+        match reader.next().await {
+            Some(message) => {
+                match message {
+                    Ok(message) => process(message),
+                    Err(e) => {
+                        debug!("message read from websocket reader was an error: {}", e);
+                        continue;
+                    }
+                };
+            }
+            None => return Ok(()),
+        }
+    }
+}
 
 async fn fallback_handler() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "endpoint not found. Try again")
